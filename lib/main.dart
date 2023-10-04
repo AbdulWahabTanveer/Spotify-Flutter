32677fdaa793714f34a53a00b305e0dc21d6d9e5
AbdulWahabTanveer <abdul.wahab.tanveer009@gmail.com>
2023-10-04 17:36:40 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       fallbackLocale: Locale('en', 'US'),
       title: 'spotify_flutter',
       initialBinding: InitialBindings(),
-      initialRoute: AppRoutes.initialRoute,
+      initialRoute: AppRoutes.appNavigationScreen,
       getPages: AppRoutes.pages,
     );
   }
